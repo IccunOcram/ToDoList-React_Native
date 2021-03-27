85e55b5d0af65265062a377395edc299ab9395ee
Marco <marconucci1@gmail.com>
2021-03-27 14:35:09 +0100

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header.js'
+import ToDoItem from './components/ToDoItem.js';
+import AddToDo from './components/AddToDo.js'
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    { text: 'Fare la spesa', key: '1'},
+    { text: 'Lavare la macchina', key: '2'},
+    { text: 'Preparare la cena', key: '3'}
+  ]);
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{
+        text: text, key: Math.random().toString()
+      },...prevTodos]
+    })
+  }
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddToDo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <ToDoItem item={item}
+              pressHandler={pressHandler}
+              />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,4 +52,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  contet: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Per far partire l'applicazione aprire la cartella della stessa usando il comando cd
+Successivamente installare le dipendenze con yarn install (per installare yarn seguire il link https://yarnpkg.com/lang/en/docs/install/)
+Successivamente per far partire l'applicazione usare il comando yarn start o expo start
